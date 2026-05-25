ec13c5c0fdaf4c12de30be749257df73a288c574
tokumikatu <t1okumikatu@gmail.com>
2026-05-26 08:57:42 +0900

    26-5-26_RTC_UP

--- run_rtc.sh
+++ run_rtc.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# 1. 正しいディレクトリに移動（現在のターミナルが開いているパスに合わせる場合）
-cd /home/keime/デスクトップ/RTCのみ/
-
-# 2. go2rtcに実行権限を与えつつ、バックグラウンド（または通常）で起動
+cd /home/avatar1/Desktop/RTC
 chmod +x ./go2rtc_linux_amd64
-./go2rtc_linux_amd64
+ ./go2rtc_linux_amd64
+#http://100.101.141.2:1984
